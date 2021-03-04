c52327f43e717a26b76a084470616700a5b84f1e
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-03-04 02:23:26 -0800

    Added repos to the page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,7 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 var getUserRepos = function (user) {
 
     //format the github api url
@@ -6,9 +10,47 @@ var getUserRepos = function (user) {
     //make a request to the url
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 };
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    console.log(event);
+    //get the value of the input
+    var username = nameInputEl.value.trim();
 
-getUserRepos("cynthia");
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = ' ';
+    }
+    else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+var displayRepos = function (repos, searchTerm) {
+    repoContainerEl.textContent = ' ';
+    repoSearchTerm.textContent = searchTerm;
+    for (let i = 0; i < repos.length; i++) {
+        //format the repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        //create a container for each repo 
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        //create a span element to hold the repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        //append to container
+        repoEl.appendChild(titleEl);
+
+        //append container to DOM
+        repoContainerEl.appendChild(repoEl);
+    }
+    console.log(repos);
+    console.log(searchTerm);
+};
+userFormEl.addEventListener("submit", formSubmitHandler);
